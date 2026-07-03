c8f7ec92ee653571486f1ad403e9c0e45c857dd4
Keshinro Tanitoluwa Joseph <tanitoluwakeshinro@gmail.com>
2026-07-03 07:18:17 +0100

    feat: add coverage reporting to CI

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
